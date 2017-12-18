dca8e42bf617842867c78bc1e6b44c277b5b5940
nsharman <nathansharman21@gmail.com>
2017-12-18 16:17:04 -0600

    Store data logging

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,12 +2,12 @@ import { combineReducers } from 'redux';
 import { routerReducer } from 'react-router-redux';
 
 import posts from './posts';
-import comments from '.comments';
+import comments from './comments';
 
-const rootReducer from combineReducers({
+const rootReducer = combineReducers({
   posts,
   comments,
-  router: routerReducer
+  routing: routerReducer
 });
 
 export default rootReducer;
