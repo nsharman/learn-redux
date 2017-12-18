62bed3d522b3a9c5ba5ce175a42a01422ebcbc36
nsharman <nathansharman21@gmail.com>
2017-12-18 15:26:49 -0600

    Initial Main setup

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return (
+      <div className="photo-grid">
+        Im the phot grid.
+      </div>
+    )
+  }
+})
+
+export default PhotoGrid;
--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,1 +1,13 @@
-// let's go!
+import React from 'react';
+import { render } from 'react-dom';
+
+// Import css.
+import css from './styles/style.styl';
+
+// Components.
+import Main from './components/Main';
+
+render(
+  <Main/>,
+  document.getElementById('root')
+);
