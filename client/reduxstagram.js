a52c2cf88f2da46d88d42a5879025216ce2c3f21
nsharman <nathansharman21@gmail.com>
2017-12-18 15:36:47 -0600

    Add routing

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 
 // Import css.
 import css from './styles/style.styl';
 
-// Components.
+// Import Components.
 import Main from './components/Main';
+import Single from './components/Single';
+import PhotoGrid from './components/PhotoGrid';
+
+const router = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
 
 render(
-  <Main/>,
+  router,
   document.getElementById('root')
 );
